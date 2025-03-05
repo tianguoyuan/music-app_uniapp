11d4d7998af0f74f267807dfb5f2896035f94ce1
tianguoyuan <603660895@qq.com>
2025-03-05 18:01:53 +0800

    icon

---
+++ src/constants/page.ts
@@ -0,0 +1,8 @@
+export const LOGIN_PATH = '/pages/user/login'
+
+export const TABBAR_PAGE_LIST = [
+  '/pages/index/index',
+  '/pages/kind/kind',
+  '/pages/shopping/shopping',
+  '/pages/user/user',
+]
