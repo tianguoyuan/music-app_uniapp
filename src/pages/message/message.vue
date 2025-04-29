e7e37c2a4521ca7b243856609773ba9e98d613f5
tianguoyuan <603660895@qq.com>
2025-04-29 17:51:43 +0800

    tabbar修改

---
+++ src/pages/message/message.vue
@@ -0,0 +1,30 @@
+<!-- 使用 type="home" 属性设置首页，其他页面不需要设置，默认为page；推荐使用json5，更强大，且允许注释 -->
+<script lang="ts" setup>
+import Tabbar from '@/components/Tabbar.vue'
+import Navbar from '@/components/Navbar.vue'
+import PLATFORM from '@/utils/platform'
+
+defineOptions({
+  name: 'Message',
+})
+onLoad(() => {
+  if (PLATFORM.isApp) uni.hideTabBar()
+})
+</script>
+<template>
+  <view class="p-3">
+    <view class="color-#fff">shopping</view>
+  </view>
+  <Tabbar tabbar-path="/pages/message/message" />
+</template>
+
+<style lang="scss" scoped></style>
+
+<route lang="json5">
+{
+  style: {
+    navigationStyle: 'custom',
+    navigationBarTitleText: '动态',
+  },
+}
+</route>
---
+++ src/pages/publish/publish.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup>
+import Navbar from '@/components/Navbar.vue'
+import Tabbar from '@/components/Tabbar.vue'
+import PLATFORM from '@/utils/platform'
+
+onLoad(() => {
+  if (PLATFORM.isApp) uni.hideTabBar()
+})
+
+function toDashboard() {
+  uni.redirectTo({
+    url: '/pages/dashboard',
+  })
+}
+</script>
+
+<template>
+  <view class="p-3">
+    <view class="color-#fff">user</view>
+  </view>
+  <Tabbar tabbar-path="/pages/publish/publish" />
+</template>
+
+<style lang="scss" scoped>
+//
+</style>
+
+<route lang="json5">
+{
+  layout: 'default',
+  style: {
+    navigationStyle: 'custom',
+    navigationBarTitleText: 'index',
+  },
+  needLogin: false,
+}
+</route>
